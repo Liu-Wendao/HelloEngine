516396fd62854b059ffbf40d0b3b892e54d5a9f8
Liu-Wendao <940237285@qq.com>
2021-12-29 10:45:14 +0800

    添加了OpenGL的Buffer抽象

--- HelloEngine/src/HelloEngine/Application.h
+++ HelloEngine/src/HelloEngine/Application.h
@@ -9,6 +9,7 @@
 #include "HelloEngine/ImGui/ImGuiLayer.h"
 
 #include "HelloEngine/Renderer/Shader.h"
+#include "HelloEngine/Renderer/Buffer.h"
 
 namespace HelloEngine
 { 
@@ -37,8 +38,10 @@ namespace HelloEngine
 		ImGuiLayer* m_ImGuiLayer;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
 		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 	private:
 		static Application* s_Instance;
 	};
---
+++ HelloEngine/src/HelloEngine/Renderer/Renderer.cpp
@@ -0,0 +1,7 @@
+#include "hepch.h"
+#include "Renderer.h"
+
+namespace HelloEngine
+{
+	RendererAPI Renderer::s_RendererAPI = RendererAPI::OpenGL;
+}
