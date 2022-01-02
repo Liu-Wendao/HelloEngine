ab4f2cacc5c9799737777002debcc7b614d7387d
Liu-Wendao <940237285@qq.com>
2022-01-02 19:17:06 +0800

    添加了BufferLayout类描述顶点缓存的布局

--- HelloEngine/src/HelloEngine/Renderer/Buffer.h
+++ HelloEngine/src/HelloEngine/Renderer/Buffer.h
@@ -2,6 +2,103 @@
 
 namespace HelloEngine
 {
+	enum class ShaderDataType
+	{
+		None = 0, 
+		Float, Float2, Float3, Float4,
+		Int, Int2, Int3, Int4,
+		Mat3, Mat4,
+		Bool
+	};
+
+	static uint32_t ShaderDataTypeSize(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case HelloEngine::ShaderDataType::Float:    return 4;
+			case HelloEngine::ShaderDataType::Float2:   return 4 * 2;
+			case HelloEngine::ShaderDataType::Float3:   return 4 * 3;
+			case HelloEngine::ShaderDataType::Float4:   return 4 * 4;
+			case HelloEngine::ShaderDataType::Int:      return 4;
+			case HelloEngine::ShaderDataType::Int2:     return 4 * 2;
+			case HelloEngine::ShaderDataType::Int3:     return 4 * 3;
+			case HelloEngine::ShaderDataType::Int4:     return 4 * 4;
+			case HelloEngine::ShaderDataType::Mat3:     return 4 * 3 * 3;
+			case HelloEngine::ShaderDataType::Mat4:     return 4 * 4 * 4;
+			case HelloEngine::ShaderDataType::Bool:     return 1;
+		}
+
+		HE_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
+	//buffer的必要属性
+	struct BufferElement
+	{
+		std::string Name;
+		ShaderDataType Type;
+		uint32_t Size;
+		uint32_t Offset;
+		bool Normalized;
+
+		BufferElement() {}
+		BufferElement(ShaderDataType type, const std::string& name, bool normalized = false)
+			:Name(name), Type(type), Size(ShaderDataTypeSize(type)), Offset(0), Normalized(normalized)
+		{}
+
+		uint32_t GetComponentCount() const
+		{
+			switch (Type)
+			{
+				case HelloEngine::ShaderDataType::Float:    return 1;
+				case HelloEngine::ShaderDataType::Float2:   return 2;
+				case HelloEngine::ShaderDataType::Float3:   return 3;
+				case HelloEngine::ShaderDataType::Float4:   return 4;
+				case HelloEngine::ShaderDataType::Int:      return 1;
+				case HelloEngine::ShaderDataType::Int2:     return 2;
+				case HelloEngine::ShaderDataType::Int3:     return 3;
+				case HelloEngine::ShaderDataType::Int4:     return 4;
+				case HelloEngine::ShaderDataType::Mat3:     return 3 * 3;
+				case HelloEngine::ShaderDataType::Mat4:     return 4 * 4;
+				case HelloEngine::ShaderDataType::Bool:     return 1;
+			}
+
+			HE_CORE_ASSERT(false, "Unknown ShaderDataType!");
+			return 0;
+		}
+
+	};
+
+	class BufferLayout
+	{
+	public:
+		BufferLayout() {}
+
+		BufferLayout(const std::initializer_list<BufferElement>& elements)
+			:m_Elements(elements)
+		{
+			CalculateOffsetsAndStride();
+		}
+
+		inline uint32_t GetStride() const { return m_Stride; }
+		inline const std::vector<BufferElement>& GetElements() const { return m_Elements; }
+
+	private:
+		void CalculateOffsetsAndStride()
+		{
+			uint32_t offset = 0;
+			for (auto& elememt : m_Elements)
+			{
+				elememt.Offset = offset;
+				offset += elememt.Size;
+				m_Stride += elememt.Size;
+			}
+		}
+	private:
+		std::vector<BufferElement> m_Elements;
+		uint32_t m_Stride = 0;
+	};
+
 	class VertexBuffer
 	{
 	public:
@@ -9,6 +106,9 @@ namespace HelloEngine
 
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
+
+		virtual const BufferLayout& GetLayout() const = 0;
+		virtual void SetLayout(const BufferLayout& layout) = 0;
 
 		static VertexBuffer* Create(float* vertices, uint32_t size);
 	};
